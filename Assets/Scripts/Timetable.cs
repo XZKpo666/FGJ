13834451ca982f10057a00f3b3ff6cb30326b4c9
XZKpo666 <hd930526@gmail.com>
2023-09-02 16:46:56 +0800

    Revert "1.0.2"
    This reverts commit 4ce9d2823e3afcad13fbb26f699d6039ae63e47b.

--- Assets/Scripts/Timetable.cs
+++ Assets/Scripts/Timetable.cs
@@ -1,25 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Timetable : MonoBehaviour
 {
-    int[] course1 = new int[8];
-    int[] course2 = new int[8];
-    int[] course3 = new int[8];
-    int[] course4 = new int[8];
-    int[] course5 = new int[8];
-
+    // Start is called before the first frame update
     void Start()
     {
-        Array.Fill(course1, 0);
-        Array.Fill(course2, 0);
-        Array.Fill(course3, 0);
-        Array.Fill(course4, 0);
-        Array.Fill(course5, 0);
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         
