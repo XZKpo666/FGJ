48c834994fb0659cb6da614d3da34f97d73fb746
Poko0000 <pokopoko130000@gmail.com>
2023-09-02 17:00:20 +0800

    1

--- Assets/Scripts/Timetable.cs
+++ Assets/Scripts/Timetable.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Timetable : MonoBehaviour
 {
+    Course[,] timeTable = new Course[5, 8];
+
     // Start is called before the first frame update
     void Start()
     {
