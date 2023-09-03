00d7202ac201e1707b514e1235ee7a5139010366
XZKpo666 <hd930526@gmail.com>
2023-09-03 11:23:35 +0800

    2.0.0

--- Assets/Scripts/ConnectGame/Judge.cs
+++ Assets/Scripts/ConnectGame/Judge.cs
@@ -1,40 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Judge : MonoBehaviour
 {
-    int arrow = 1;
-
-    void Start()
-    {
-        
-    }
+    public int arrow = 1;
+    public bool CanJudge = true;
 
     void Update()
     {
         Judgement();
     }
+
     void Judgement()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow) && arrow == 1)
+        if (CanJudge == true)
         {
-            Debug.Log("Work1");
-            arrow++;
-        }
-        else if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Debug.Log("gameOver");
-        }
+            if (arrow == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    Debug.Log("Work1");
+                    StartCoroutine(JudgeDelay());
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    Debug.Log("gameOver");
+                }
+            }
+          
+            if (arrow == 2)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    Debug.Log("Work2");
+                    StartCoroutine(JudgeDelay());
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    Debug.Log("gameOver2");
+                }
+            }           
+        }       
+    }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && arrow == 2)
-        {
-            Debug.Log("Work2");
-            arrow++;
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Debug.Log("gameOver");
-        }
+    IEnumerator JudgeDelay()
+    {
+        CanJudge = false;
+        yield return new WaitForSeconds(0.1f);
+        arrow++;
+        CanJudge = true;
     }
 }
